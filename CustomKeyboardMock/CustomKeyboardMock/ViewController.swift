95c32e877c7753d144511979072af1a6dc74bdcb
“v15a1” <visalrajapakse@gmail.com>
2022-02-09 22:32:12 +0530

    first commit

---
+++ CustomKeyboardMock/CustomKeyboardMock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomKeyboardMock
+//
+//  Created by Visal Rajapakse on 2022-02-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
