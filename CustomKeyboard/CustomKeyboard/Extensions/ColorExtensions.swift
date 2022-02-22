c72af1074bafd2bc6028aaefdb0b2becaf547b14
“v15a1” <visalrajapakse@gmail.com>
2022-02-22 23:02:00 +0530

    Completed basic calculator app

---
+++ CustomKeyboard/CustomKeyboard/Extensions/ColorExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  CustomKeyboard
+//
+//  Created by Visal Rajapakse on 2022-02-22.
+//
+
+import UIKit
+
+// MARK: Color extension
+extension UIColor {
+    static let calculatorOrange = UIColor(named: "CalculatorOrange")
+}
